0ae2b832cbdc3e8071e7c6ab5edd6cfcd769fe97
soumadeepsengupta <soumadeep.sengupta11032004@gmail.com>
2025-06-21 03:36:05 +0530

    Updated Bento Grid layout

--- deepdesigns/src/App.jsx
+++ deepdesigns/src/App.jsx
@@ -26,33 +26,28 @@ function App() {
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       {/* Responsive bento grid layout */}
       <main
-        className="w-full max-w-none mx-0 px-2 py-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 md:max-w-3xl xl:max-w-[1440px] md:mx-auto"
+        className="w-full max-w-none mx-0 px-2 py-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-12 gap-4 md:max-w-3xl xl:max-w-[1440px] md:mx-auto"
         style={{ minHeight: 'calc(100vh - 160px)' }}
       >
-        {/* Desktop/tablet: grid, Mobile: stack */}
-        <div className="flex flex-col gap-4 h-full md:col-span-1">
-          <div className="flex-1 min-h-[340px] xl:min-h-[420px]">
-            <IntroCard />
-          </div>
-          <div className="flex-1 min-h-[200px] xl:min-h-[220px]">
-            <SkillsCard />
-          </div>
+        {/* Row 1: IntroCard | PhotoCard | WorkCard (WorkCard spans 2 rows on xl, full width on md) */}
+        <div className="min-h-[340px] xl:min-h-[420px] h-full xl:col-span-5 xl:row-span-1">
+          <IntroCard />
         </div>
-        <div className="flex flex-col gap-4 h-full md:col-span-1">
-          <div className="flex-1 min-h-[340px] xl:min-h-[420px]">
-            <PhotoCard darkMode={darkMode} />
-          </div>
-          <div className="flex-1 min-h-[200px] xl:min-h-[220px]">
-            <ContactCard darkMode={darkMode} />
-          </div>
+        <div className="min-h-[340px] xl:min-h-[420px] h-full xl:col-span-3 xl:row-span-1">
+          <PhotoCard darkMode={darkMode} />
         </div>
-        <div className="flex flex-col h-full md:col-span-1">
-          <div className="flex-1 min-h-[560px] xl:min-h-[660px]">
-            <WorkCard />
-          </div>
+        <div className="min-h-[340px] xl:min-h-[660px] h-full md:col-span-2 xl:col-span-4 xl:row-span-2">
+          <WorkCard />
+        </div>
+        {/* Row 2: SkillsCard | ContactCard */}
+        <div className="min-h-[200px] xl:min-h-[220px] h-full xl:col-span-4 xl:row-span-1">
+          <SkillsCard />
+        </div>
+        <div className="min-h-[200px] xl:min-h-[220px] h-full xl:col-span-4 xl:row-span-1">
+          <ContactCard darkMode={darkMode} />
         </div>
       </main>
-      {/* On mobile, SocialLinksFooter is always full width and directly below WorkCard */}
+      {/* Footer always at the bottom, full width */}
       <footer className="w-full max-w-none mx-0 px-2 pb-6 md:max-w-3xl xl:max-w-[1440px] md:mx-auto">
         <SocialLinksFooter />
       </footer>
